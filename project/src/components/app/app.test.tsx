5e3a3248786d2ffeb347f3fa560a6153ec152404
Aleksey Titov <talexden@gmail.com>
2021-11-01 23:21:15 +0300

    2.11.10 выполняет module2-task2

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,8 +1,12 @@
 import {render, screen} from '@testing-library/react';
 import App from './app';
 
+const TEMPORARY_DATA = {
+  count: 5,
+};
+
 test('Renders app-component', () => {
-  render(<App />);
+  render(<App count={TEMPORARY_DATA.count}/>);
   const textElement = screen.getByText(/Hello, world!/i);
   expect(textElement).toBeInTheDocument();
 });
