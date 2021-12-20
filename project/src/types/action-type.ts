6b76fe42570c72193e1dd2bd4fe0e1414cb25a2d
alex <talexden@gmail.com>
2021-12-20 09:23:08 +0300

    refactor actions.type

--- project/src/types/action-type.ts
+++ project/src/types/action-type.ts
@@ -1,29 +1,7 @@
 import {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {AxiosInstance} from 'axios';
 import {StateType} from './state-type';
-import {
-  loadOffers,
-  loadOfferById,
-  setSelectedOffer,
-  setCity,
-  setCityOffers,
-  setComment,
-  requireAuthorization,
-  requireLogout,
-  redirectToRoute,
-  sortCityOffers,
-  loadNearbyOffers,
-  loadCommentsByOfferAction,
-  loadUserInfo,
-  setFavorite,
-  loadFavorites,
-  loadFavoriteOffer,
-  replaceOffer,
-  clearCommentForm,
-  postOfferCommentRequest,
-  postOfferCommentSuccess
-} from '../store/action';
-
+import {Action} from 'redux';
 
 export enum ActionType {
   RedirectToRoute = 'route/redirectToRoute',
@@ -48,30 +26,7 @@ export enum ActionType {
   PostOfferCommentSuccess = 'app/postOfferCommentSuccess',
 }
 
+export type ThunkActionResult<R = Promise<void>> = ThunkAction<R, StateType, AxiosInstance, Action>;
 
-export type Actions =
-  | ReturnType<typeof setCity>
-  | ReturnType<typeof setCityOffers>
-  | ReturnType<typeof setComment>
-  | ReturnType<typeof postOfferCommentRequest>
-  | ReturnType<typeof postOfferCommentSuccess>
-  | ReturnType<typeof setSelectedOffer>
-  | ReturnType<typeof setFavorite>
-  | ReturnType<typeof loadFavoriteOffer>
-  | ReturnType<typeof replaceOffer>
-  | ReturnType<typeof loadFavorites>
-  | ReturnType<typeof loadOffers>
-  | ReturnType<typeof loadOfferById>
-  | ReturnType<typeof loadNearbyOffers>
-  | ReturnType<typeof loadCommentsByOfferAction>
-  | ReturnType<typeof requireAuthorization>
-  | ReturnType<typeof requireLogout>
-  | ReturnType<typeof sortCityOffers>
-  | ReturnType<typeof loadUserInfo>
-  | ReturnType<typeof clearCommentForm>
-  | ReturnType<typeof redirectToRoute>;
-
-export type ThunkActionResult<R = Promise<void>> = ThunkAction<R, StateType, AxiosInstance, Actions>;
-
-export type ThunkAppDispatch = ThunkDispatch<StateType, AxiosInstance, Actions>;
+export type ThunkAppDispatch = ThunkDispatch<StateType, AxiosInstance, Action>;
 
